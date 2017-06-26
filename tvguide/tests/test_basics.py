bcb8f3f75c80d648d9eb2bc882779ca097e05b22
mrtommyb <tom@tombarclay.com>
2017-06-26 09:56:57 -0700

    adding test

---
+++ tvguide/tests/test_basics.py
@@ -0,0 +1,13 @@
+
+"""Basic sanity checks to verify that tvguide works.
+To run, simply type "py.test".
+"""
+
+
+def test_import():
+    """Can we import tvguide successfully?"""
+    import tvguide
+
+
+    
+
